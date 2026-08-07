6ed0966c1edc555af4ee99882dd22ee8a000baa1
Manjula Perera <manjulaperera@users.noreply.github.com>
2026-08-07 22:22:29 +1000

    Add XML comment to Parse method

--- src/CodingTest/Parsing/IntegerSequenceParser.cs
+++ src/CodingTest/Parsing/IntegerSequenceParser.cs
@@ -4,6 +4,11 @@ namespace CodingTest.Parsing;
 
 internal static class IntegerSequenceParser
 {
+    /// <summary>
+    /// Parses a space-separated sequence of integers from the given input string.
+    /// </summary>
+    /// <param name="input">The input string containing integers separated by spaces.</param>
+    /// <returns>An array of parsed integers.</returns>
     public static int[] Parse(string input)
     {
         ArgumentNullException.ThrowIfNull(input);
